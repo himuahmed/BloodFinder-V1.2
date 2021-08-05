926a74d7aa29e9cb1e575d8238c1e4dad26c77cb
Himu <mohsi.ahamed@gmail.com>
2021-08-06 03:08:26 +0600

    message status updates based on user interactions.

--- src/app/auth/auth.routing.ts
+++ src/app/auth/auth.routing.ts
@@ -1,4 +1,4 @@
-import { Routes, RouterModule } from '@angular/router';
+/* import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { AuthModule } from './auth.module';
 import { LoginComponent } from './login/login.component';
@@ -7,11 +7,9 @@ import { RegistrationComponent } from './registration/registration.component';
 const routes: Routes = [
   { 
     path:'',component:AuthComponent
-  /*   children:[
-      {path:'login',component:LoginComponent},
-      {path:'signup',component:RegistrationComponent},
-    ] */
+
    },
 ];
 
 export const AuthRoutes = RouterModule.forChild(routes);
+ */
